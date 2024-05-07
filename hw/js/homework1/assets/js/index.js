f2253a84b61a68cdddf1c713c0297661e3a277da
daria <traskovskaad48@gmail.com>
2024-05-07 22:26:20 +0200

    tasks variable

---
+++ hw/js/homework1/assets/js/index.js
@@ -0,0 +1,30 @@
+let a = 4;
+let b = 5;
+console.log(a * b);
+
+let c = 3;
+let d = 2;
+console.log(c / d);
+
+let e = 6;
+let f = 20;
+console.log(e + f);
+
+let numberVariable = 11;
+let booleanVariable = true;
+let stringVariable = "java script";
+let stringnumberVariable = "100";
+console.log(numberVariable);
+console.log(booleanVariable);
+console.log(stringVariable);
+console.log(stringnumberVariable);
+
+let num = 1;
+num += 11;
+num -= 11;
+num *= 11;
+num /= 11;
+num++;
+num--;
+alert(num);
+console.log(num);
