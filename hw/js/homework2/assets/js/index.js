f517ac7b49835ac08d8bd4f623d0be96636fe970
daria <traskovskaad48@gmail.com>
2024-05-13 19:19:38 +0200

    output numbers_decrease

---
+++ hw/js/homework2/assets/js/index.js
@@ -0,0 +1,18 @@
+let startNumber = 25;
+const endNumber = 0;
+
+while (startNumber >= endNumber) {
+  console.log(startNumber);
+  startNumber--;
+}
+
+let number = 25;
+
+do {
+  console.log(number);
+  number--;
+} while (number >= 0);
+
+for (let i = 25; i >= 0; i--) {
+  console.log(i);
+}
