4d6bdb330b47cbcf22b3ca8e49155fb4167bea5e
daria <traskovskaad48@gmail.com>
2024-05-13 21:05:00 +0200

    output numbers % 5

--- hw/js/homework2/assets/js/index.js
+++ hw/js/homework2/assets/js/index.js
@@ -16,3 +16,28 @@ do {
 for (let i = 25; i >= 0; i--) {
   console.log(i);
 }
+
+let a = 10;
+const b = 50;
+
+while (a <= b) {
+  if (a % 5 === 0) {
+    console.log(a);
+  }
+  a++;
+}
+
+let c = 10;
+const d = 50;
+do {
+  if (c % 5 === 0) {
+    console.log(c);
+  }
+  c++;
+} while (c <= d);
+
+for (e = 10; e <= 50; e++) {
+  if (e % 5 === 0) {
+    console.log(e);
+  }
+}
