2a60ca517657dcd044e9ae616b6871d341cf8984
daria <traskovskaad48@gmail.com>
2024-05-15 16:42:47 +0200

    use if and switch

--- hw/js/homework3/assets/js/index.js
+++ hw/js/homework3/assets/js/index.js
@@ -39,3 +39,49 @@ if (purchaseAmount > 500 && purchaseAmount <= 800) {
 let endSum = purchaseAmount * discount;
 
 alert(endSum);
+
+// switch-конструкція
+
+let daysWeek = prompt("Введіть номер тижня");
+
+switch (daysWeek) {
+  case "1":
+    alert("Понеділок");
+    break;
+  case "2":
+    alert("Вівторок");
+    break;
+  case "3":
+    alert("Середа");
+    break;
+  case "4":
+    alert("Четверг");
+    break;
+  case "5":
+    alert("Пятниця");
+    break;
+  case "6":
+    alert("Субота");
+    break;
+  case "7":
+    alert("Неділя");
+    break;
+  default:
+    alert("Дня з таким номером не існує");
+}
+
+const day = +prompt("Введіть число від 1 до 31");
+
+switch (true) {
+  case day >= 1 && day <= 10:
+    alert("Перша декада");
+    break;
+  case day >= 11 && day <= 20:
+    alert("Друга декада");
+    break;
+  case day >= 21 && day <= 31:
+    alert("Третя декада");
+    break;
+  default:
+    alert("Такого дня не має");
+}
